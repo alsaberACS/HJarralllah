73606ea8fbbf756a5f09201f35d65afc6a569cb4
alsaberACS <admin@acs-kw.com>
2018-09-30 03:26:58 +0100

    Hessa V3

--- Database.R
+++ Database.R
@@ -33,3 +33,16 @@ DAPS <- read.spss("DAP.sav", use.value.label=TRUE, to.data.frame=TRUE)
 DCOS <- read.spss("DCO.sav", use.value.label=TRUE, to.data.frame=TRUE)
 QUS <- read.spss("Questionnaire.sav", use.value.label=TRUE, to.data.frame=TRUE)
 #---------------Rename variables---------- (Step 3)
+explanatory = c("BMI1", "BMI2", "AHI", "Sex")
+dependent = "Meno"
+DAPS %>%
+  summary_factorlist(dependent, explanatory, p=TRUE)
+
+glm(depdendent ~ explanatory, family="binomial")
+
+explanatory = c("BMI1", "BMI2")
+dependent = "Sex"
+DAPS %>%
+  finalfit(dependent, explanatory, p=TRUE)
+
+
